606ee3d25a47436f956a9eb47f8aa3af0627a9a7
Mehr <you@example.com>
2019-07-24 14:23:19 +0430

    Genetic algorithm function file added

---
+++ Genetic algorithm function.m
@@ -0,0 +1,11 @@
+%% Titel
+% Genetic algorithm function
+
+%% cleanings :)
+clc
+clear
+close all
+
+%% Genetic algorithm function
+
+% xopt = ga(Fitness_function, number_of_variable, A_Inequalities, b_Inequalities, A_Equal_Constraints, b_Equal_Constraints, lower_bound, upper_bound, nonlinear_constraints, integer_design_variable, options);
