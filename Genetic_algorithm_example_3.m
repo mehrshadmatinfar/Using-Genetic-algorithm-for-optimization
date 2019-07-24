373289d624d55e8b46c2672344108b87f8786b41
Mehr <you@example.com>
2019-07-25 00:32:05 +0430

    Genetic algorithm function added

--- Genetic_algorithm_example_3.m
+++ Genetic_algorithm_example_3.m
@@ -8,14 +8,20 @@ clear
 close all
 
 %% Introduc a function
+number_of_variable = 2;
+
 lower_bound = [0;
                0];
 upper_bound = [200;
                200];
 
-A_Inequalities = [1/3, 1;
+A_Inequalities = [1/3, 1; 
                   0.1, -1];
 b_Inequalities = [4.5;
-                  0 ];
+                   0 ];
 
 Fitness_function = @(x) (-8+x(1))^2 + (-2+x(2))^2;
+
+%% Genetic algorithm function
+
+[xopt, yopt] = ga(Fitness_function, number_of_variable, A_Inequalities, b_Inequalities, [], [], lower_bound, upper_bound)
