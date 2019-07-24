10c27ed19b216918d7aeb6e42ac048b419d9dbea
Mehr <you@example.com>
2019-07-24 23:28:48 +0430

    GA added

--- Genetic_algorithm_example_2.m
+++ Genetic_algorithm_example_2.m
@@ -8,6 +8,13 @@ clear
 close all
 
 %% Introduc a function
-lower_bound = [0, 0];
+lower_bound = [0; 0];
 
 Fitness_function = @(x) 100*(x(2)-x(1).^2).^2 + (1-x(1)).^2;
+
+%% Genetic algorithm function
+
+xopt = ga(Fitness_function, 2, [], [], [], [], lower_bound, []);
+
+disp(xopt(1))
+disp(xopt(2))
