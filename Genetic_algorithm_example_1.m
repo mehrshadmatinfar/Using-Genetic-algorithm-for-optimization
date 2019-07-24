04668a6383b1e98c7520a2bcd203c752375ca25b
Mehr <you@example.com>
2019-07-24 13:16:43 +0430

    Genetic algorithm function added to search for optimum point

--- Genetic_algorithm_example_1.m
+++ Genetic_algorithm_example_1.m
@@ -15,3 +15,8 @@ Fitness_function = @(x) x.^4 - 50*x.^3 - 1800*x.^2 - 800*x;
 
 plot(xx, Fitness_function(xx))
 grid on
+
+%% Genetic algorithm function
+xopt = ga(Fitness_function, 1);
+% we use 1 because it has only "x" as variable
+yopt = Fitness_function(xopt);
