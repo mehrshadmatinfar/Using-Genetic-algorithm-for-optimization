df8b5ac124190d4882adadfcc9b41ebef9e4db2e
Mehr <you@example.com>
2019-07-25 14:05:20 +0430

    Multi objective added to Genetic algorithm function.m file

--- Genetic algorithm function.m
+++ Genetic algorithm function.m
@@ -9,3 +9,5 @@ close all
 %% Genetic algorithm function
 
 % [xopt, yopt, exit_flag, out1, Population, score] = ga(Fitness_function, number_of_variable, A_Inequalities, b_Inequalities, A_Equal_Constraints, b_Equal_Constraints, lower_bound, upper_bound, nonlinear_constraints, integer_design_variable, options);
+
+% [xopt, yopt, exit_flag, out1, Population, score] = gamultiobj(@Fitness_function, number_of_variable, A_Inequalities, b_Inequalities, A_Equal_Constraints, b_Equal_Constraints, lower_bound, upper_bound, nonlinear_constraints, integer_design_variable, options);
