0c05d0870ae21c5e7937f75e55519f2cbdc35ff2
Mehr <you@example.com>
2019-07-25 11:35:58 +0430

    nonlinear_constraints_example file updated and output variables added

--- Genetic algorithm function.m
+++ Genetic algorithm function.m
@@ -8,4 +8,4 @@ close all
 
 %% Genetic algorithm function
 
-% xopt = ga(Fitness_function, number_of_variable, A_Inequalities, b_Inequalities, A_Equal_Constraints, b_Equal_Constraints, lower_bound, upper_bound, nonlinear_constraints, integer_design_variable, options);
+% [xopt, yopt, exit_flag, out1, Population, score] = ga(Fitness_function, number_of_variable, A_Inequalities, b_Inequalities, A_Equal_Constraints, b_Equal_Constraints, lower_bound, upper_bound, nonlinear_constraints, integer_design_variable, options);
