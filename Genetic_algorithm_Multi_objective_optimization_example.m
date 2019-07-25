43fcb0c01da143149a0d83b52028b83f4ace6dd5
Mehr <you@example.com>
2019-07-25 14:18:28 +0430

    Genetic algorithm function added

--- Genetic_algorithm_Multi_objective_optimization_example.m
+++ Genetic_algorithm_Multi_objective_optimization_example.m
@@ -7,14 +7,18 @@ clear
 close all
 
 %% Introduc a function
-number_of_variable = 3;
+number_of_variable = 2;
 
-lower_bound = [0;
-               0;
-               0];
-upper_bound = [10;
-               10;
-               10];
+lower_bound = [];
+upper_bound = [];
 
 A_Inequalities = [];
 b_Inequalities = [];
+
+%% Genetic algorithm function
+
+[xopt, yopt, exit_flag, out1, Population, score] = gamultiobj(...
+    @Genetic_algorithm_Multi_objective_optimization_example_Ff, ...
+    number_of_variable, A_Inequalities, b_Inequalities, [], [], ...
+    lower_bound, upper_bound, ...
+    Genetic_algorithm_Multi_objective_optimization_example_NC(x));
