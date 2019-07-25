98fa8facf77ef4ac2b8733d9abe1cdd7881a39a7
Mehr <you@example.com>
2019-07-25 08:53:56 +0430

    function interduced

--- nonlinear_constraints_example.m
+++ nonlinear_constraints_example.m
@@ -5,3 +5,18 @@
 clc
 clear
 close all
+
+%% Introduc a function
+number_of_variable = 3;
+
+lower_bound = [0;
+               0;
+               0];
+upper_bound = [10;
+               10;
+               10];
+
+A_Inequalities = [];
+b_Inequalities = [];
+
+Fitness_function = @(x) (-100 - (x(1)-5)^2 - (x(2)-5)^2 + (x(3)-5)^2)/100;
